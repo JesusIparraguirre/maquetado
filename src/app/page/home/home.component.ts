a82f54b3ff5a38dd8304a90c5a5ca607d587a820
Jesus Iparraguirre <jesusiparraguirre1998@gmail.com>
2021-08-27 05:51:45 -0500

    footer home v2

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  i = [1,2,3];
   constructor() { }
 
   ngOnInit(): void {
